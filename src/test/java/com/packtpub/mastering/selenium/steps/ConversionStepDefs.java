4412b01e6bdfc207ee85973821edbf9268745438
Daita <sdaita@uspto.gov>
2016-12-26 10:25:47 -0500

    a

--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -1,11 +1,7 @@
 package com.packtpub.mastering.selenium.steps;
 
-import cucumber.api.java.en.Given;
-
 /**
  * Created by sdaita on 12/26/2016.
  */
 public class ConversionStepDefs {
-    @Given("^I want to convert (\\d+).(\\d+) degree F ")
-
 }
