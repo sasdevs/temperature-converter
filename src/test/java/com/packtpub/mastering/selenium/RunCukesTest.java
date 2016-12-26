dd0b2ec1037e42920b1a839859ea8f6dedafe9ec
Daita <sdaita@uspto.gov>
2016-12-26 14:49:22 -0500

    x

--- src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
+++ src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
@@ -1,14 +1,17 @@
 package com.packtpub.mastering.selenium;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber ;
+import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 /**
  * Created by sdaita on 12/25/2016.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="src/test/resources", format = {"pretty", "html:target/cucumber-html"})
+@CucumberOptions(
+        features ="src/test/resources",
+        format = {"pretty", "html:target/cucumber-html"}
+)
 
 public class RunCukesTest {
 }
--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -1,9 +1,8 @@
 package com.packtpub.mastering.selenium.steps;
 
 import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.en.*;
+
 
 /**
  * Created by sdaita on 12/26/2016.
