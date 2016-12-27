7090d28404f01795c9a5f01872ba5396d38cd25c
Daita <sdaita@uspto.gov>
2016-12-26 20:18:11 -0500

    copy from riponalwasim

--- src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
+++ src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
@@ -1,17 +1,17 @@
 package com.packtpub.mastering.selenium;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import cucumber.junit.Cucumber;
 
 /**
  * Created by sdaita on 12/25/2016.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@Cucumber.Options(
         features ="src/test/resources",
-        format = {"pretty", "html:target/cucumber-html"}
-)
+        format = {"pretty", "html:target/cucumber-htmlreport",
+        "json-pretty:target/cucumber-report.json"})
 
 public class RunCukesTest {
 }
+
--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -1,29 +1,47 @@
 package com.packtpub.mastering.selenium.steps;
+import com.packtpub.mastering.selenium.pageobjects.Google;
+import com.packtpub.mastering.selenium.pageobjects.TemperatureConverterPage;
+import cucumber.annotation.After;
+import cucumber.annotation.Before;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 
 /**
  * Created by sdaita on 12/26/2016.
  */
+
 public class ConversionStepDefs {
-    @Given("^I want to convert (\\d+)\\.(\\d+) F to C$")
-    public void i_want_to_convert_F_to_C(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    private WebDriver driver;
+    public Google googlePage;
+    public TemperatureConverterPage temperatureConverterPage;
+    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
+    public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        googlePage = new Google(driver);
+        temperatureConverterPage = googlePage.goToTemperatureConversionPage();
+        temperatureConverterPage = new TemperatureConverterPage(driver);
     }
 
-    @When("^I input the value of F as (\\d+)\\.(\\d+) in text field$")
-    public void i_input_the_value_of_F_as_in_text_field(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
+    public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
+        //input Fahrenheit value
+        temperatureConverterPage.inputFahrenheit(98.6);
     }
 
-    @Then("^it should be converted to C as (\\d+) degrees$")
-    public void it_should_be_converted_to_C_as_degrees(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^It should be converted to Celsius as (\\d+) degree$")
+    public void It_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
+        //assertion
+        assertEquals(temperatureConverterPage.actualResult(), temperatureConverterPage.expectedResult());
+        driver.quit();
     }
-
 }
